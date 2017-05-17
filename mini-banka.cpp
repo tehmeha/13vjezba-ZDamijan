69e177d5d33464d19ab575e3e85f90370649ca7f
ZDamijan <zlatko.damijanic@gmail.com>
2017-05-17 15:45:35 +0200

    Update mini-banka.cpp

--- mini-banka.cpp
+++ mini-banka.cpp
@@ -3,12 +3,34 @@ using namespace std;
 
 int main()
 {
-    cout << "Glavni izbornik" << endl;
-    cout << "1. Unos nove osobe" << endl;
-    cout << "2. Ispisi podatke" << endl;
-    cout << "3. pretraga prema racunu" << endl;
-    cout << "4. Pretraga prema prezimenu" << endl;
-    cout << "5. Ispisi sortirano" << endl;
-    int izbor;
-    cin >> izbor;
+    while(1)
+    {
+        cout << "Glavni izbornik" << endl;
+        cout << "1. Unos nove osobe" << endl;
+        cout << "2. Ispisi podatke" << endl;
+        cout << "3. pretraga prema racunu" << endl;
+        cout << "4. Pretraga prema prezimenu" << endl;
+        cout << "5. Ispisi sortirano" << endl;
+        int izbor;
+        int brojKlijenata = 0;
+        int MAX = 50;
+        cin >> izbor;
+        if( izbor == 1 )
+        {
+        }
+        else if( izbor == 2 )
+        {
+        }
+        else if( izbor == 3 )
+        {
+        }
+        else if( izbor == 4 )
+        {
+        }
+        else if( izbor == 5 )
+        {
+            cout << "Kraj rada" << ednl;
+            break;
+        }
+    }
 }
