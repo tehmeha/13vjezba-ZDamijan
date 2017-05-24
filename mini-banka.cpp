18185b9f7fabc774a2e7bd3c660501f98d8e1b80
ZDamijan <zlatko.damijanic@gmail.com>
2017-05-24 12:17:08 +0200

    Update mini-banka.cpp

--- mini-banka.cpp
+++ mini-banka.cpp
@@ -8,12 +8,14 @@ int main()
         cout << "Glavni izbornik" << endl;
         cout << "1. Unos nove osobe" << endl;
         cout << "2. Ispisi podatke" << endl;
-        cout << "3. pretraga prema racunu" << endl;
-        cout << "4. Pretraga prema prezimenu" << endl;
-        cout << "5. Ispisi sortirano" << endl;
+        cout << "3. Pretraga prema racunu" << endl;
+        cout << "4. Pretraga prema prezimenu i imenu" << endl;
+        cout << "5. Ispisi sortirano prema prezimenu i imenu" << endl;
+        cout << "6. Izlaz iz programa" << endl;
         int izbor;
         int brojKlijenata = 0;
         int MAX = 50;
+        cout << "Upisite broj iz izbornika: " << endl;
         cin >> izbor;
         if( izbor == 1 )
         {
@@ -29,8 +31,15 @@ int main()
         }
         else if( izbor == 5 )
         {
+        }
+        else if( izbor == 6 )
+        {
             cout << "Kraj rada" << ednl;
             break;
+        }
+        else
+        {
+            cout << "Unijeli ste krivi broj." << endl;
         }
     }
 }
